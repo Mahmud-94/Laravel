7f5a7ac01a00f8bdd537dc93d67b2321e6c6ec6d
Mahmud-94 <mahamudulhassanredoy@gmail.com>
2024-12-07 02:18:51 +0600

    5 dec

--- laravel_sirproject/resources/views/backend/layouts/app.blade.php
+++ laravel_sirproject/resources/views/backend/layouts/app.blade.php
@@ -1,76 +1,76 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8" />
-	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
-	<title>Kenny I Fast build Admin dashboard for any platform</title>
-	<meta name="description" content="Kenny is a Dashboard & Admin Site Responsive Template by hencework." />
-	<meta name="keywords" content="admin, admin dashboard, admin template, cms, crm, Kenny Admin, kennyadmin, premium admin templates, responsive admin, sass, panel, software, ui, visualization, web app, application" />
-	<meta name="author" content="hencework"/>
-<<<<<<< HEAD
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+    <title>Kenny I Fast build Admin dashboard for any platform</title>
+    <meta name="description" content="Kenny is a Dashboard & Admin Site Responsive Template by hencework." />
+    <meta name="keywords"
+        content="admin, admin dashboard, admin template, cms, crm, Kenny Admin, kennyadmin, premium admin templates, responsive admin, sass, panel, software, ui, visualization, web app, application" />
+    <meta name="author" content="hencework" />
 
-	@include ('backend.layouts.css');
-=======
-	
-	
-	@yield('css');
->>>>>>> 0fa476b51e57971cc6d7f718523751d714c0eed1
+
+    @include ('backend.layouts.css');
+
+
+
+    @yield('css');
+
 </head>
 
 <body>
-	
+
     <div class="wrapper">
-			<!-- Top Menu Items -->
+        <!-- Top Menu Items -->
 
-			@if (Auth()->guard('admin')->check())
-			@include ('backend.layouts.topmenu')
+        @if (Auth()->guard('admin')->check())
+            @include ('backend.layouts.topmenu')
 
-			@elseif(Auth()->guard('doctor')->check())
-			@include ('backend.layouts.doctor_topmenu')
+        @elseif(Auth()->guard('doctor')->check())
+            @include ('backend.layouts.doctor_topmenu')
 
-			@endif
+        @endif
 
 
 
-			<!-- /Top Menu Items -->
+        <!-- /Top Menu Items -->
 
-			<!-- Left Sidebar Menu -->
-			@if (Auth()->guard('admin')->check())
-			@include ('backend.layouts.left_sidebar')
+        <!-- Left Sidebar Menu -->
+        @if (Auth()->guard('admin')->check())
+            @include ('backend.layouts.left_sidebar')
 
-			@elseif(Auth()->guard('doctor')->check())
-<<<<<<< HEAD
-			@include ('backend.layouts.doctor_left_sidebar')
+        @elseif(Auth()->guard('doctor')->check())
 
-=======
-			@include ('backend.layouts.left_sidebar')
-			
->>>>>>> 0fa476b51e57971cc6d7f718523751d714c0eed1
-			@endif
+            @include ('backend.layouts.doctor_left_sidebar')
 
-			<!-- /Left Sidebar Menu -->
 
-			<!-- Right Sidebar Menu -->
-			@include ('backend.layouts.right_sidebar');
-			<!-- /Right Sidebar Menu -->
-			<div class="page-wrapper">
-        <!-- Main Content -->
-		
-		@yield('content');
-        <!-- /Main Content -->
-			</div>
+            @include ('backend.layouts.left_sidebar')
+
+        @endif
+
+        <!-- /Left Sidebar Menu -->
+
+        <!-- Right Sidebar Menu -->
+        @include ('backend.layouts.right_sidebar');
+        <!-- /Right Sidebar Menu -->
+        <div class="page-wrapper">
+            <!-- Main Content -->
+
+            @yield('content');
+            <!-- /Main Content -->
+        </div>
 
     </div>
     <!-- /#wrapper -->
 
-	<!-- JavaScript -->
-<<<<<<< HEAD
+    <!-- JavaScript -->
+    <<<<<<< HEAD @include ('backend.layouts.js');
+        =======
 
-    @include ('backend.layouts.js');
-=======
-	
-    
-	@yield('js')
->>>>>>> 0fa476b51e57971cc6d7f718523751d714c0eed1
+
+        @yield('js')
+        >>>>>>> 0fa476b51e57971cc6d7f718523751d714c0eed1
 </body>
+
 </html>
